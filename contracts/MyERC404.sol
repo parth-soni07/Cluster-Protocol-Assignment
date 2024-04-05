381a45de94cc17b02eb6be10f5d946e2245a9554
parth-soni07 <202151104@iiitvadodara.ac.in>
2024-04-06 03:03:54 +0530

    changed Mytoken

--- contracts/MyERC404.sol
+++ contracts/MyERC404.sol
@@ -7,19 +7,15 @@ import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 contract MyERC404 is ERC404 {
     using Strings for uint256;
 
-    string public dataURI;
     string public baseTokenURI;
     address public _owner = msg.sender;
+
     constructor() ERC404("MyERC404", "ME404", 18, 10000, _owner) {
         balanceOf[_owner] = 10000 * 10 ** 18;
     }
 
-    function setDataURI(string memory _dataURI) public onlyOwner {
-        dataURI = _dataURI;
-    }
-
-    function setTokenURI(string memory _tokenURI) public onlyOwner {
-        baseTokenURI = _tokenURI;
+    function setBaseTokenURI(string memory _baseTokenURI) public onlyOwner {
+        baseTokenURI = _baseTokenURI;
     }
 
     function setNameSymbol(
@@ -31,7 +27,8 @@ contract MyERC404 is ERC404 {
 
     function tokenURI(uint256 id) public view override returns (string memory) {
         if (bytes(baseTokenURI).length > 0) {
-            return string.concat("baseTokenURI is: ", baseTokenURI);
+            return string.concat(id.toString(), baseTokenURI);
         }
+        return "";
     }
 }
