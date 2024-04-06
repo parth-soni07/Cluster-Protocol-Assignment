abf21cae459bd47a9a56922a3cb1bf0c8b76c79c
parth-soni07 <202151104@iiitvadodara.ac.in>
2024-04-06 22:31:29 +0530

    added tokenSupply Increment

--- contracts/MyERC4907.sol
+++ contracts/MyERC4907.sol
@@ -1,17 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
 import "./lib/ERC4907.sol";
+
 
 contract MyERC4907 is ERC4907 {
 
+    uint256 private _totalSupply;
     constructor(string memory name, string memory symbol)
      ERC4907(name,symbol)
      {         
      }
 
-    function mint(uint256 tokenId, address to) public {
-        _mint(to, tokenId);
+    function mint(address to) public {
+        _totalSupply++;
+        uint256 _tokenId = _totalSupply;
+        _mint(to, _tokenId);
     }
 
 }
